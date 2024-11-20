185f10ec5c32611074201d43179a33dced91fd65
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2024-11-20 15:08:19 +0100

    docs: workflows sample with a basic deduplication

--- samples/transfer-workflow-compensation/src/main/java/com/example/wallet/domain/Wallet.java
+++ samples/transfer-workflow-compensation/src/main/java/com/example/wallet/domain/Wallet.java
@@ -1,12 +1,50 @@
 package com.example.wallet.domain;
 
-public record Wallet(String id, int balance) {
+import com.example.wallet.application.WalletEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  public Wallet withdraw(int amount) {
-    return new Wallet(id, balance - amount);
+import java.util.ArrayList;
+import java.util.List;
+
+public record Wallet(String id, int balance, List<String> commandIds) {
+
+  private static final Logger logger = LoggerFactory.getLogger(WalletEntity.class);
+  public static Wallet EMPTY = new Wallet("", 0, new ArrayList<>());
+
+  public boolean isEmpty(){
+    return id.equals("");
   }
 
-  public Wallet deposit(int amount) {
-    return new Wallet(id, balance + amount);
+  public List<WalletEvent> handle(WalletCommand command) {
+    // a very basic dedupliction mechanism, make sure to put some constraints on the commandIds list size
+    // for instance based on the number of commands you expect to be processed
+    // and based on the time window you expect to process them in
+    if (commandIds.contains(command.commandId())) {
+      logger.info("Command already processed: [{}]", command.commandId());
+      return List.of();
+    }
+    return switch (command) {
+      case WalletCommand.Deposit deposit ->
+        List.of(new WalletEvent.Deposited(command.commandId(), deposit.amount()));
+      case WalletCommand.Withdraw withdraw ->
+        List.of(new WalletEvent.Withdrawn(command.commandId(), withdraw.amount()));
+    };
+  }
+
+
+  public Wallet applyEvent(WalletEvent event) {
+    return switch (event) {
+      case WalletEvent.Created created ->
+        new Wallet(created.walletId(), created.initialBalance(), new ArrayList<>());
+      case WalletEvent.Withdrawn withdrawn -> {
+        commandIds.add(withdrawn.commandId());
+        yield new Wallet(id, balance - withdrawn.amount(), commandIds);
+      }
+      case WalletEvent.Deposited deposited -> {
+        commandIds.add(deposited.commandId());
+        yield new Wallet(id, balance + deposited.amount(), commandIds);
+      }
+    };
   }
 }
