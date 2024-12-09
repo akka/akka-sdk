e02cc1ecbdcf768007dfd58aed32a25f705e36f1
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-12-09 17:40:28 +0100

    remove some unused

--- akka-javasdk/src/main/scala/akka/javasdk/impl/keyvalueentity/KeyValueEntitiesImpl.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/keyvalueentity/KeyValueEntitiesImpl.scala
@@ -7,21 +7,16 @@ package akka.javasdk.impl.keyvalueentity
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.annotation.InternalApi
-import akka.javasdk.Metadata
-import akka.javasdk.Tracing
-import akka.javasdk.impl.AbstractContext
-import akka.javasdk.impl.ActivatableContext
 import akka.javasdk.impl.Service
 import akka.javasdk.impl.Settings
 import akka.javasdk.impl.serialization.JsonSerializer
-import akka.javasdk.impl.telemetry.SpanTracingImpl
-import akka.javasdk.keyvalueentity.CommandContext
 import akka.javasdk.keyvalueentity.KeyValueEntity
 import akka.javasdk.keyvalueentity.KeyValueEntityContext
 import akka.stream.scaladsl.Source
-import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.Tracer
 import kalix.protocol.value_entity._
+
+// FIXME remove
 
 /**
  * INTERNAL API
@@ -49,30 +44,3 @@ private[impl] final class KeyValueEntitiesImpl(
 
   override def handle(in: Source[ValueEntityStreamIn, NotUsed]): Source[ValueEntityStreamOut, NotUsed] = ???
 }
-
-/**
- * INTERNAL API
- */
-@InternalApi
-private[impl] final class CommandContextImpl(
-    override val entityId: String,
-    override val commandName: String,
-    override val commandId: Long,
-    override val metadata: Metadata,
-    span: Option[Span],
-    tracerFactory: () => Tracer)
-    extends AbstractContext
-    with CommandContext
-    with ActivatableContext {
-
-  override def tracing(): Tracing =
-    new SpanTracingImpl(span, tracerFactory)
-}
-
-/**
- * INTERNAL API
- */
-@InternalApi
-private[impl] final class KeyValueEntityContextImpl(override val entityId: String, system: ActorSystem)
-    extends AbstractContext
-    with KeyValueEntityContext
