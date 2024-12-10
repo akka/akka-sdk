7ba537875104d956e0cd8d1a20fa696efe156245
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-12-10 08:18:34 +0100

    chore: Misc cleanup
    * Remove EventSourcedEntitiesImpl
    * Remove KeyValueEntitiesImpl
    * Remove ActionsImpl
    * Collect spi descriptors in one loop
    * Remove commandId

--- akka-javasdk/src/main/java/akka/javasdk/keyvalueentity/CommandContext.java
+++ akka-javasdk/src/main/java/akka/javasdk/keyvalueentity/CommandContext.java
@@ -21,7 +21,10 @@ public interface CommandContext extends MetadataContext {
    * The id of the command being executed.
    *
    * @return The id of the command.
+   *
+   * @deprecated not used anymore
    */
+  @Deprecated
   long commandId();
 
   /**
