f0eabc2ca258d67be42c12c1d40d09dc8a86ce0e
Johan Andrén <johan@markatta.com>
2025-01-24 15:44:20 +0100

    fromatting

--- akka-javasdk/src/main/java/akka/javasdk/http/HttpClientProvider.java
+++ akka-javasdk/src/main/java/akka/javasdk/http/HttpClientProvider.java
@@ -15,13 +15,14 @@ public interface HttpClientProvider {
   /**
    * Returns a {@link HttpClient} to interact with the specified HTTP service.
    *
-   * <p>If the serviceName is a service name without protocol or domain the client will be configured to connect to
-   * another service deployed with that name on the same Akka project. The runtime will take care of
-   * routing requests to the service and keeping the data safe by encrypting the connection between services and identifying
-   * the client as coming from this service.
+   * <p>If the serviceName is a service name without protocol or domain the client will be
+   * configured to connect to another service deployed with that name on the same Akka project. The
+   * runtime will take care of routing requests to the service and keeping the data safe by
+   * encrypting the connection between services and identifying the client as coming from this
+   * service.
    *
-   * <p>If it is a full dns name prefixed with "http://" or "https://" it will connect to services available on the
-   * public internet.
+   * <p>If it is a full dns name prefixed with "http://" or "https://" it will connect to services
+   * available on the public internet.
    */
   HttpClient httpClientFor(String serviceName);
 }
