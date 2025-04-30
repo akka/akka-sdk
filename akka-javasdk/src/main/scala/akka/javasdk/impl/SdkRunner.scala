19c9ce64a7546362a82f00b8d31aa12063543b54
Johan Andrén <johan@markatta.com>
2025-04-30 14:47:03 +0200

    Started playing around with A2A

--- akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
@@ -639,8 +639,9 @@ private final class Sdk(
       sdkExecutionContext.asInstanceOf[Executor]
   }
 
-  private val serviceExplorationMcpEndpoint =
-    McpServiceExplorationEndpoint(httpEndpointDescriptors, grpcEndpointDescriptors)(system)
+  private val additionalAiEndpoints =
+    McpServiceExplorationEndpoint(httpEndpointDescriptors, grpcEndpointDescriptors)(system) +: A2A.dummyA2aEndpoint()(
+      system.executionContext)
 
   val spiComponents: SpiComponents = {
 
@@ -666,8 +667,8 @@ private final class Sdk(
         timedActionDescriptors ++
         consumerDescriptors ++
         viewDescriptors ++
-        workflowDescriptors :+
-        serviceExplorationMcpEndpoint)
+        workflowDescriptors ++
+        additionalAiEndpoints)
         .filterNot(isDisabled(combinedDisabledComponents))
 
     val preStart = { (_: ActorSystem[_]) =>
