4e6430ec3c32d67af73583ce6e95ce938fcc634a
Johan Andrén <johan@markatta.com>
2025-05-16 15:44:49 +0200

    started on user API

---
+++ akka-javasdk-annotation-processor-tests/mcp-endpoints-descriptors/src/main/java/com/example/BasicMcpEndpoint.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import akka.javasdk.annotations.mcp.McpEndpoint;
+import akka.javasdk.annotations.mcp.McpTool;
+
+@McpEndpoint(
+    serverName = "my-mcp-server",
+    serverVersion = "1.0",
+    instructions = "Some special care is needed when using this example mcp endpoint"
+)
+class BasicMcpEndpoint {
+
+  public BasicMcpEndpoint() {}
+
+  @McpTool(
+      name = "echo",
+      description = "Echoes back whatever string is sent to it")
+  public String echoTool(String someParam) {
+    return someParam;
+  }
+
+}
---
+++ akka-javasdk-annotation-processor-tests/mcp-endpoints-descriptors/src/test/scala/com/example/McpEndpointsDescriptorSpec.scala
@@ -0,0 +1,17 @@
+package com.example
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class McpEndpointsDescriptorSpec extends AnyWordSpec with Matchers {
+  "akka-javasdk-components.conf" should {
+    "have correct configuration" in {
+      val config = ConfigFactory.load("META-INF/akka-javasdk-components.conf")
+
+      val keyValueComponents = config.getStringList("akka.javasdk.components.mcp-endpoint")
+      keyValueComponents.size() shouldBe 1
+      keyValueComponents should contain("com.example.BasicMcpEndpoint")
+    }
+  }
+}
