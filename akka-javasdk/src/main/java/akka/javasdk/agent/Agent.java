fa028797b7802d941283a4660d1afef77ba5ce06
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2025-05-21 15:55:37 +0200

    PR comments

--- akka-javasdk/src/main/java/akka/javasdk/agent/Agent.java
+++ akka-javasdk/src/main/java/akka/javasdk/agent/Agent.java
@@ -75,7 +75,7 @@ public abstract class Agent {
 
       /**
        * Create a system message from a template. Call @{@link PromptTemplate} before to initiate or update template value.
-       * Provide arguments that will be applied to the template using Java {@link String#format} method.
+       * Provide arguments that will be applied to the template using Java {@link String#formatted} method.
        * @param templateId the id of the template to use
        * @param args the arguments to apply to the template
        */
