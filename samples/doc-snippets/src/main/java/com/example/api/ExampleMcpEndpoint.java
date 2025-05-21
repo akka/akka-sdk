1e7f1ad34e1faf29b23d9d1b2fb3d8d59b3fdd3c
Johan Andrén <johan@markatta.com>
2025-05-21 17:41:46 +0200

    updated example mcp enpoint

--- samples/doc-snippets/src/main/java/com/example/api/ExampleMcpEndpoint.java
+++ samples/doc-snippets/src/main/java/com/example/api/ExampleMcpEndpoint.java
@@ -1,19 +1,33 @@
 package com.example.api;
 
-import akka.javasdk.annotations.mcp.McpEndpoint;
-import akka.javasdk.annotations.mcp.McpResource;
-import akka.javasdk.annotations.mcp.McpTool;
-import akka.javasdk.annotations.mcp.McpToolParameterDescription;
+import akka.javasdk.annotations.Acl;
+import akka.javasdk.annotations.mcp.*;
+import akka.javasdk.client.ComponentClient;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-@McpEndpoint(serverName = "doc-snippets-mcp-sample", serverVersion = "0.0.1")
+// tag::endpoint-class[]
+@Acl(allow = @Acl.Matcher(principal = Acl.Principal.ALL))
+@McpEndpoint(
+    serverName = "doc-snippets-mcp-sample",
+    serverVersion = "0.0.1")
 public class ExampleMcpEndpoint {
+  private ComponentClient componentClient;
 
+  public ExampleMcpEndpoint(ComponentClient componentClient) {
+    this.componentClient = componentClient;
+  }
+  // end::endpoint-class[]
+
+
+  // tag::tool-manual-input-schema[]
   public record EchoToolRequest(String message) {}
 
-  @McpTool(name = "echo", description = "Echoes back whatever string is thrown at it", inputSchema = """
+  @McpTool(
+      name = "echo",
+      description = "Echoes back whatever string is thrown at it",
+      inputSchema = """
       {
         "type":"object",
         "properties": {
@@ -21,26 +35,35 @@ public class ExampleMcpEndpoint {
          },
          "required": ["message"]
       }
-      """)
+      """ // <1>
+    )
   public String echo(EchoToolRequest input) {
     return input.message;
   }
+  // end::tool-manual-input-schema[]
 
 
+  // tag::tool[]
   // example of reflectively deduced input schema, still needs descriptions
   public record EchoToolRequest2(
-      @McpToolParameterDescription("The first number")
+      @McpToolParameterDescription("The first number") // <1>
       int n1,
       @McpToolParameterDescription("The second number")
       int n2
       ) {}
 
-  @McpTool(name = "multiply", description = "Multiplies the two given numbers and returns the result")
+  @McpTool(
+      name = "multiply", // <2>
+      description = "Multiplies the two given numbers and returns the result",
+      annotations = {ToolAnnotation.ReadOnly, ToolAnnotation.ClosedWorld} // <3>
+  )
   public String multiply(EchoToolRequest2 input) {
     var result = input.n1 * input.n2;
     return Integer.toString(result);
   }
+  // end::tool[]
 
+  // tag::resources[]
   @McpResource(uri = "file://background.png", name = "Background image", description = "A background image for Akka sites", mimeType = "image/png")
   public byte[] backgroundImage() {
     try (InputStream in = this.getClass().getResourceAsStream("/static-resources/images/background.png")) {
@@ -50,4 +73,8 @@ public class ExampleMcpEndpoint {
       throw new RuntimeException(e);
     }
   }
+  // end::resources[]
+
+  // tag::endpoint-class[]
 }
+// end::endpoint-class[]
