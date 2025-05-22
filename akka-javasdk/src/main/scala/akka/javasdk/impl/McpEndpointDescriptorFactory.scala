62909ffa1735e62cb7d49042402c954fa39b2306
Johan Andrén <johan@markatta.com>
2025-05-22 17:14:41 +0200

    Protocol handling moved to runtime (not compiling because of runtime version)

--- akka-javasdk/src/main/scala/akka/javasdk/impl/McpEndpointDescriptorFactory.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/McpEndpointDescriptorFactory.scala
@@ -6,19 +6,39 @@ package akka.javasdk.impl
 
 import akka.actor.typed.ActorSystem
 import akka.annotation.InternalApi
-import akka.http.scaladsl.model.MediaTypes
+import akka.javasdk.annotations.Acl
+import akka.javasdk.annotations.JWT
 import akka.javasdk.annotations.mcp.McpEndpoint
 import akka.javasdk.annotations.mcp.McpResource
 import akka.javasdk.annotations.mcp.McpTool
 import akka.javasdk.annotations.mcp.McpToolParameterDescription
 import akka.javasdk.annotations.mcp.ToolAnnotation
-import akka.javasdk.impl.Mcp.BlobResourceContents
-import akka.javasdk.impl.Mcp.Implementation
-import akka.javasdk.impl.Mcp.TextResourceContents
+import akka.javasdk.impl.AclDescriptorFactory.deriveAclOptions
+import akka.javasdk.impl.JwtDescriptorFactory.deriveJWTOptions
+import akka.javasdk.impl.serialization.JsonSerializer
 import akka.parboiled2.util.Base64
-import akka.runtime.sdk.spi.HttpEndpointDescriptor
+import akka.runtime.sdk.spi.ComponentOptions
+import akka.runtime.sdk.spi.McpEndpointConstructionContext
+import akka.runtime.sdk.spi.McpEndpointDescriptor
+import akka.runtime.sdk.spi.McpEndpointDescriptor.BlobResourceContents
+import akka.runtime.sdk.spi.McpEndpointDescriptor.Implementation
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaBoolean
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaDataType
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaInteger
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaNumber
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaObject
+import akka.runtime.sdk.spi.McpEndpointDescriptor.JsonSchemaString
+import akka.runtime.sdk.spi.McpEndpointDescriptor.Resource
+import akka.runtime.sdk.spi.McpEndpointDescriptor.ResourceMethodDescriptor
+import akka.runtime.sdk.spi.McpEndpointDescriptor.ResponseContent
+import akka.runtime.sdk.spi.McpEndpointDescriptor.TextContent
+import akka.runtime.sdk.spi.McpEndpointDescriptor.TextResourceContents
+import akka.runtime.sdk.spi.McpEndpointDescriptor.ToolDescription
+import akka.runtime.sdk.spi.McpEndpointDescriptor.ToolMethodDescriptor
+import akka.runtime.sdk.spi.MethodOptions
 import org.slf4j.LoggerFactory
 
+import java.lang.reflect.Field
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.util.Optional
@@ -35,7 +55,7 @@ object McpEndpointDescriptorFactory {
 
   def apply[T](mcpEndpointClass: Class[T], instanceFactory: () => T)(implicit
       system: ActorSystem[_],
-      sdkExecutor: ExecutionContext): HttpEndpointDescriptor = {
+      sdkExecutor: ExecutionContext): McpEndpointDescriptor = {
 
     val endpointAnnotation = mcpEndpointClass.getAnnotation(classOf[McpEndpoint])
 
@@ -53,14 +73,15 @@ object McpEndpointDescriptorFactory {
         throw new IllegalArgumentException(
           s"MCP tool methods must accept 0 or 1 parameters, but ${method.getName} accepts ${method.getParameterCount}")
 
-      val inputSchema =
+      val inputSchema: JsonSchemaObject =
         if (annotation.inputSchema().isBlank) {
           // FIXME reflectively infer schema from type
           //   what do we support, only object, or also individual parameters (and use their names)?
-          if (method.getParameterCount == 0) Mcp.InputSchema(properties = Map.empty, required = Seq.empty)
+          if (method.getParameterCount == 0)
+            new JsonSchemaObject(properties = Map.empty, required = Seq.empty, description = None)
           else inputSchemaFor(method.getParameterTypes.head)
         } else {
-          JsonRpc.Serialization.mapper.readValue(annotation.inputSchema(), classOf[Mcp.InputSchema])
+          JsonSerializer.internalObjectMapper.readValue(annotation.inputSchema(), classOf[JsonSchemaObject])
         }
 
       val toolName =
@@ -69,10 +90,10 @@ object McpEndpointDescriptorFactory {
 
       val toolAnnotations = toolAnnotationsFor(mcpEndpointClass, method.getName, annotation.annotations().toVector)
 
-      val toolDescription = Mcp.ToolDescription(toolName, annotation.description(), inputSchema, toolAnnotations)
+      val toolDescription = new ToolDescription(toolName, annotation.description(), inputSchema, toolAnnotations)
 
-      val callback = (params: Map[String, Any]) =>
-        Future[Mcp.CallToolResult] {
+      val callback = (constructionContext: McpEndpointConstructionContext, params: Map[String, Any]) =>
+        Future[ResponseContent] {
           val endpointInstance = instanceFactory.apply()
           val returnValue = if (method.getParameterCount == 0) {
             // FIXME fail on input params when expecting none
@@ -80,24 +101,26 @@ object McpEndpointDescriptorFactory {
           } else {
             // FIXME fail on no input (can we know with manually specified schema?)
             // FIXME handle required fields and input validation correctly
-            val parsedInput = JsonRpc.Serialization.mapper.convertValue(params, method.getParameterTypes.head)
+            val parsedInput = JsonSerializer.internalObjectMapper.convertValue(params, method.getParameterTypes.head)
             method.invoke(endpointInstance, parsedInput)
           }
           returnValue match {
-            case text: String => Mcp.CallToolResult(Seq(Mcp.TextContent(text)))
+            case text: String => new TextContent(text)
             case unknown      =>
               // FIXME cover with validation
               // FIXME handle/allow audio and image
               throw new RuntimeException(
                 s"Unsupported tool return value (${if (unknown == null) "null" else unknown.getClass.toString}")
           }
-
         }
 
-      toolDescription -> callback
+      new ToolMethodDescriptor(
+        toolDescription = toolDescription,
+        method = callback,
+        methodOptions = new MethodOptions(None, None))
     }
 
-    val toolsByName = tools.groupBy { case (desc, _) => desc.name }
+    val toolsByName = tools.groupBy { _.toolDescription.name }
     val duplicateTools = toolsByName.collect { case (name, tools) if tools.size > 1 => name }.toSeq
     if (duplicateTools.nonEmpty) {
       throw ValidationException(
@@ -116,7 +139,7 @@ object McpEndpointDescriptorFactory {
         throw new IllegalArgumentException(
           s"MCP resources must be of 0 arity, but ${method.getName} has a non empty parameter list")
 
-      val resourceDescription = Mcp.Resource(
+      val resourceDescription = new Resource(
         uri = annotation.uri(),
         name = annotation.name(),
         description = Some(annotation.description()).filter(!_.isBlank),
@@ -124,100 +147,100 @@ object McpEndpointDescriptorFactory {
         annotations = None,
         size = None)
 
-      val callback = { () =>
+      val callback = { (constructionContext: McpEndpointConstructionContext) =>
         val endpointInstance = instanceFactory()
         val result = method.invoke(endpointInstance)
         result match {
           case bytes: Array[Byte] =>
             val base64Encoded = Base64.rfc2045().encodeToString(bytes, lineSep = false)
-            Seq(
-              BlobResourceContents(
-                base64Encoded,
-                resourceDescription.uri,
-                resourceDescription.mimeType.getOrElse(MediaTypes.`application/octet-stream`.value)))
+            Seq(new BlobResourceContents(base64Encoded, resourceDescription.uri, resourceDescription.mimeType))
           case text: String =>
-            Seq(
-              TextResourceContents(
-                text,
-                resourceDescription.uri,
-                mimeType = resourceDescription.mimeType.getOrElse(MediaTypes.`text/plain`.value)))
+            Seq(new TextResourceContents(text, resourceDescription.uri, mimeType = resourceDescription.mimeType))
         }
       }
 
-      resourceDescription -> callback
+      new ResourceMethodDescriptor(resourceDescription, new MethodOptions(None, None), callback)
     }
 
-    val resourcesByUri = resources.groupBy { case (res, _) => res.uri }
+    val resourcesByUri = resources.groupBy { res => res.resource.uri }
     val duplicateUris = resourcesByUri.collect { case (name, resources) if resources.size > 1 => name }
     if (duplicateUris.nonEmpty) {
       throw ValidationException(
         s"MCP resources with duplicated names exist in ${mcpEndpointClass.getName}: [${duplicateUris.mkString(", ")}], make sure that each resource has a separate, unique URI")
     }
 
-    new Mcp.StatelessMcpEndpoint(
-      Mcp.McpDescriptor(
-        implementation =
-          Implementation(name = endpointAnnotation.serverName(), version = endpointAnnotation.serverVersion()),
-        resources = resources,
-        resourceTemplates = Seq.empty,
-        tools = tools,
-        instructions = endpointAnnotation.instructions()))
-      .httpEndpoint(endpointAnnotation.path())
+    new McpEndpointDescriptor(
+      endpointPath = endpointAnnotation.path(),
+      implementationName = mcpEndpointClass.getName,
+      implementation =
+        new Implementation(name = endpointAnnotation.serverName(), version = endpointAnnotation.serverVersion()),
+      instructions = endpointAnnotation.instructions(),
+      resources = resources,
+      resourceTemplates = Seq.empty, // FIXME user API for these
+      prompts = Seq.empty, // FIXME user API for these
+      tools = tools,
+      componentOptions = new ComponentOptions(
+        deriveAclOptions(Option(mcpEndpointClass.getAnnotation(classOf[Acl]))),
+        deriveJWTOptions(Option(mcpEndpointClass.getAnnotation(classOf[JWT])), mcpEndpointClass.getCanonicalName)))
   }
 
-  private[impl] def inputSchemaFor(value: Class[_]): Mcp.InputSchema = {
-    val properties = value.getDeclaredFields.toSeq.map { field =>
+  private[impl] def inputSchemaFor(value: Class[_]): JsonSchemaObject = {
+    val properties = value.getDeclaredFields.toVector.map { field: Field =>
       val description = field.getAnnotation(classOf[McpToolParameterDescription]) match {
         case null =>
           log.info(
             "Field [{}] is missing a tool description, client LLMs may not understand the purpose of the field, add one using {}",
             classOf[McpToolParameterDescription].getName,
             field.getName)
-          ""
+          None
         case annotation =>
-          annotation.value()
+          Some(annotation.value())
       }
 
-      val (jsonFieldType, optional) = jsonSchemaTypeFor(field.getGenericType)
-      field.getName -> (Mcp.ToolProperty(`type` = jsonFieldType, description = description), optional)
+      field.getName -> jsonSchemaTypeFor(field.getGenericType, description)
     }.toMap
 
-    Mcp.InputSchema(
+    new JsonSchemaObject(
+      description = None,
       properties = properties.map { case (key, (toolProperty, _)) => key -> toolProperty },
       required = properties.collect { case (key, (_, optional)) if !optional => key }.toSeq)
   }
 
-  private final val typeNameMap = Map(
-    "short" -> "number",
-    "byte" -> "number",
-    "char" -> "number",
-    "int" -> "number",
-    "long" -> "number",
-    "double" -> "number",
-    "float" -> "number",
-    "boolean" -> "boolean",
-    "java.lang.Short" -> "number",
-    "java.lang.Byte" -> "number",
-    "java.lang.Char" -> "number",
-    "java.lang.Integer" -> "number",
-    "java.lang.Long" -> "number",
-    "java.lang.Double" -> "number",
-    "java.lang.Float" -> "number",
-    "java.lang.Boolean" -> "boolean")
+  private def number(description: Option[String]) = new JsonSchemaNumber(description)
+  private def integer(description: Option[String]) = new JsonSchemaInteger(description)
+  private def boolean(description: Option[String]) = new JsonSchemaBoolean(description)
 
-  private def jsonSchemaTypeFor(genericFieldType: Type): (String, Boolean) = {
+  private final val typeNameMap: Map[String, Option[String] => JsonSchemaDataType] = Map(
+    "short" -> integer,
+    "byte" -> integer,
+    "char" -> integer,
+    "int" -> integer,
+    "long" -> integer,
+    "double" -> number,
+    "float" -> number,
+    "boolean" -> boolean,
+    "java.lang.Short" -> integer,
+    "java.lang.Byte" -> integer,
+    "java.lang.Char" -> integer,
+    "java.lang.Integer" -> integer,
+    "java.lang.Long" -> integer,
+    "java.lang.Double" -> number,
+    "java.lang.Float" -> number,
+    "java.lang.Boolean" -> boolean)
+
+  private def jsonSchemaTypeFor(genericFieldType: Type, description: Option[String]): (JsonSchemaDataType, Boolean) = {
     typeNameMap.get(genericFieldType.getTypeName) match {
-      case Some(jsTypeName) => (jsTypeName, false)
+      case Some(jsTypeFactory) => (jsTypeFactory(description), false)
       case None =>
         val clazz = genericFieldType match {
           case c: Class[_]          => c
           case p: ParameterizedType => p.getRawType.asInstanceOf[Class[_]]
         }
-        if (clazz == classOf[String]) ("string", false)
+        if (clazz == classOf[String]) (new JsonSchemaString(description), false)
         else {
           genericFieldType match {
             case p: ParameterizedType if clazz == classOf[Optional[_]] =>
-              val (jsonFieldType, _) = jsonSchemaTypeFor(p.getActualTypeArguments.head)
+              val (jsonFieldType, _) = jsonSchemaTypeFor(p.getActualTypeArguments.head, description)
               (jsonFieldType, true)
             case other =>
               // FIXME support collections
@@ -231,7 +254,7 @@ object McpEndpointDescriptorFactory {
   private def toolAnnotationsFor(
       endpointClass: Class[?],
       methodName: String,
-      annotations: Seq[ToolAnnotation]): Option[Mcp.ToolAnnotation] = {
+      annotations: Seq[ToolAnnotation]): Option[McpEndpointDescriptor.ToolAnnotation] = {
     if (annotations.isEmpty) None
     else {
       val set = annotations.toSet
@@ -245,7 +268,7 @@ object McpEndpointDescriptorFactory {
       }
 
       Some(
-        Mcp.ToolAnnotation(
+        new McpEndpointDescriptor.ToolAnnotation(
           destructive = annotationPresent(ToolAnnotation.Destructive, ToolAnnotation.NonDestructive),
           idempotent = annotationPresent(ToolAnnotation.Idempotent, ToolAnnotation.NonIdempotent),
           openWorld = annotationPresent(ToolAnnotation.OpenWorld, ToolAnnotation.ClosedWorld),
