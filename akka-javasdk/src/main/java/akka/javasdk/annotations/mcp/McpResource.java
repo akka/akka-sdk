496d801d645b8ab53920dbe5dcd731508d96bb33
Johan Andrén <johan@markatta.com>
2025-05-28 17:29:24 +0200

    more work

--- akka-javasdk/src/main/java/akka/javasdk/annotations/mcp/McpResource.java
+++ akka-javasdk/src/main/java/akka/javasdk/annotations/mcp/McpResource.java
@@ -16,8 +16,25 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface McpResource {
+  /**
+   * @return A unique URI identifying the resource.
+   */
   String uri();
+
+  /**
+   * @return A human-readable name for this resource. Clients can use this to populate UI elements, for example.
+   */
   String name();
-  String description();
-  String mimeType();
+
+  /**
+   * @return A description of what this resource represents. Clients can use this to improve the LLM's
+   *         understanding of available resources. It can be thought of as a "hint" to the model.
+   */
+  String description() default  "";
+
+  /**
+   * @return The MIME type of this resource. If not defined, String output will be presented as {@code text/plain} and
+   *         byte arrays as {@code ???}
+   */
+  String mimeType() default "";
 }
---
+++ akka-javasdk/src/main/java/akka/javasdk/mcp/McpRequestContext.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2021-2024 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.javasdk.mcp;
+
+import akka.annotation.DoNotInherit;
+import akka.grpc.javadsl.Metadata;
+import akka.javasdk.JwtClaims;
+import akka.javasdk.Principals;
+import akka.javasdk.Tracing;
+
+/**
+ * Not for user extension, can be injected as constructor parameter into MCP endpoint components
+ */
+@DoNotInherit
+public interface McpRequestContext {
+  /**
+   * Get the principals associated with this request.
+   *
+   * @return The principals associated with this request.
+   */
+  Principals getPrincipals();
+
+  /** @return The JWT claims, if any, associated with this request. */
+  JwtClaims getJwtClaims();
+
+  /** Access to tracing for custom app specific tracing. */
+  Tracing tracing();
+}
