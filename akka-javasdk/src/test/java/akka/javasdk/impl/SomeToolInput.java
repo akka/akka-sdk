004755ececd34bb638c29374d93ea12d755540bd
Johan Andrén <johan@markatta.com>
2025-05-28 09:44:11 +0200

    vale, formatting

--- akka-javasdk/src/test/java/akka/javasdk/impl/SomeToolInput.java
+++ akka-javasdk/src/test/java/akka/javasdk/impl/SomeToolInput.java
@@ -10,33 +10,30 @@ import java.util.List;
 import java.util.Optional;
 
 interface SomeToolInput {
-  record SomeToolInput1(@Description("some description")
-                              String string,
-                              Optional<String> optionalString,
-                              boolean booleanPrimitive,
-                              int intPrimitive,
-                              long longPrimitive,
-                              double doublePrimitive,
-                              float floatPrimitive,
-                              byte bytePrimitive,
-                              Boolean boxedBoolean,
-                              Integer boxedInteger,
-                              Long boxedLong,
-                              Double boxedDouble,
-                              Float boxedFloat,
-                              Byte boxedByte
-  ) { }
+  record SomeToolInput1(
+      @Description("some description") String string,
+      Optional<String> optionalString,
+      boolean booleanPrimitive,
+      int intPrimitive,
+      long longPrimitive,
+      double doublePrimitive,
+      float floatPrimitive,
+      byte bytePrimitive,
+      Boolean boxedBoolean,
+      Integer boxedInteger,
+      Long boxedLong,
+      Double boxedDouble,
+      Float boxedFloat,
+      Byte boxedByte) {}
 
-  record SomeToolInput2(
-      @Description("some strings")
-      List<String> listOfStrings) {}
+  record SomeToolInput2(@Description("some strings") List<String> listOfStrings) {}
 
-  record SomeToolInput3(
-      @Description("a nested object")
-      NestedObject nestedObject) {}
-  record NestedObject(
-      @Description("a value in the nested object")
-      String someString) {}
+  record SomeToolInput3(@Description("a nested object") NestedObject nestedObject) {}
 
-  String someMethod(@Description("some tool input") SomeToolInput1 input1, SomeToolInput2 input2, SomeToolInput3 input3);
+  record NestedObject(@Description("a value in the nested object") String someString) {}
+
+  String someMethod(
+      @Description("some tool input") SomeToolInput1 input1,
+      SomeToolInput2 input2,
+      SomeToolInput3 input3);
 }
