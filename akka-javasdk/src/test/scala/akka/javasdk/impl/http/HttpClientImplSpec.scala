76ca2c8d54bf61ca4a3bd0450395e341e0682862
Renato Cavalcanti <renato@cavalcanti.be>
2025-06-05 21:22:54 +0200

    fix: http-client returns String without decoding (#510)
    * fix: http-client returns String without decoding
    
    * added tests

--- akka-javasdk/src/test/scala/akka/javasdk/impl/http/HttpClientImplSpec.scala
+++ akka-javasdk/src/test/scala/akka/javasdk/impl/http/HttpClientImplSpec.scala
@@ -7,6 +7,8 @@ package akka.javasdk.impl.http
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.javadsl.model.HttpHeader
+import akka.javasdk.JsonSupport
+import akka.javasdk.http.HttpResponses
 import akka.javasdk.http.RequestBuilder
 import akka.util.ByteString
 import org.scalatest.matchers.should.Matchers
@@ -23,6 +25,58 @@ class HttpClientImplSpec extends AnyWordSpec with Matchers {
           .addQueryParameter("another", "name")
       }
       builder.request.getUri.toString shouldBe "http://test.com/test?key=some+value&another=name"
+    }
+
+    "get a String as raw string" in new HttpClientImplSuite {
+
+      val builder: RequestBuilderImpl[ByteString] = get(identity)
+      val res =
+        builder
+          .bodyParserInternal(classOf[String], HttpResponses.ok("foo"), ByteString.fromString("foo"))
+          .body()
+
+      res shouldBe "foo"
+    }
+
+    "get a json payload as raw string" in new HttpClientImplSuite {
+
+      val builder: RequestBuilderImpl[ByteString] = get(identity)
+      case class Foo(value: String)
+      val foobar = Foo("bar")
+      val jsonPayload = JsonSupport.encodeToAkkaByteString(foobar)
+
+      val res =
+        builder
+          .bodyParserInternal(classOf[String], HttpResponses.ok(foobar), jsonPayload)
+          .body()
+
+      res shouldBe jsonPayload.utf8String
+    }
+
+    "get an integer as raw string" in new HttpClientImplSuite {
+
+      val builder: RequestBuilderImpl[ByteString] = get(identity)
+      val jsonPayload = JsonSupport.encodeToAkkaByteString(10)
+
+      val res =
+        builder
+          .bodyParserInternal(classOf[String], HttpResponses.ok(10), jsonPayload)
+          .body()
+
+      res shouldBe "10"
+    }
+
+    "get a boolean as raw string" in new HttpClientImplSuite {
+
+      val builder: RequestBuilderImpl[ByteString] = get(identity)
+      val jsonPayload = JsonSupport.encodeToAkkaByteString(true)
+
+      val res =
+        builder
+          .bodyParserInternal(classOf[String], HttpResponses.ok(true), jsonPayload)
+          .body()
+
+      res shouldBe "true"
     }
   }
 
@@ -43,6 +97,7 @@ trait HttpClientImplSuite {
     builder(client.GET(url))
       .asInstanceOf[RequestBuilderImpl[ByteString]]
   }
+
   def get(builder: RequestBuilder[ByteString] => RequestBuilder[ByteString]): RequestBuilderImpl[ByteString] =
     get(path)(builder)
 }
