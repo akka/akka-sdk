54d60eea2fe4704b11629cde968a0a8da64a922e
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-06-10 16:12:09 +0200

    docs: Update Ask Akka guide
    * Changed the flow to start with the agent, then add indexing and RAG

--- samples/doc-snippets/src/main/java/com/example/application/AskAkkaAgent.java
+++ samples/doc-snippets/src/main/java/com/example/application/AskAkkaAgent.java
@@ -1,14 +1,13 @@
-package akka.ask.agent.application;
+package com.example.application;
 
+// tag::class[]
 import akka.javasdk.agent.Agent;
 import akka.javasdk.annotations.AgentDescription;
 import akka.javasdk.annotations.ComponentId;
 
-// tag::class[]
-@ComponentId("ask-akka-agent")
+@ComponentId("ask-akka-agent") // <2>
 @AgentDescription(name = "Ask Akka", description = "Expert in Akka")
-public class AskAkkaAgent extends Agent {
-  private final Knowledge knowledge;
+public class AskAkkaAgent extends Agent { // <1>
 
   private static final String SYSTEM_MESSAGE =
       """
@@ -16,18 +15,12 @@ public class AskAkkaAgent extends Agent {
       Given the following sections from the Akka SDK documentation, answer the question using only that information, outputted in markdown format.
       If you are unsure and the text is not explicitly written in the documentation, say:
       Sorry, I don't know how to help with that.
-      """.stripIndent(); // <1>
+      """.stripIndent(); // <4>
 
-  public AskAkkaAgent(Knowledge knowledge) { // <2>
-    this.knowledge = knowledge;
-  }
-
-  public StreamEffect ask(String question) {
-    var enrichedQuestion = knowledge.addKnowledge(question); // <3>
-
+  public StreamEffect ask(String question) { // <3>
     return streamEffects()
-        .systemMessage(SYSTEM_MESSAGE)
-        .userMessage(enrichedQuestion) // <4>
+        .systemMessage(SYSTEM_MESSAGE) // <4>
+        .userMessage(question) // <5>
         .thenReply();
   }
 
