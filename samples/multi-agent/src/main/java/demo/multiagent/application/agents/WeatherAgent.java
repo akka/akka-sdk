476b9042fff59546c247fb2e474f353e17f2e195
Renato Cavalcanti <renato@cavalcanti.io>
2025-06-16 19:09:44 +0200

    removed unused imports

--- samples/multi-agent/src/main/java/demo/multiagent/application/agents/WeatherAgent.java
+++ samples/multi-agent/src/main/java/demo/multiagent/application/agents/WeatherAgent.java
@@ -3,12 +3,8 @@ package demo.multiagent.application.agents;
 import akka.javasdk.agent.Agent;
 import akka.javasdk.annotations.AgentDescription;
 import akka.javasdk.annotations.ComponentId;
-import akka.javasdk.annotations.Description;
 import akka.javasdk.annotations.FunctionTool;
-import akka.javasdk.http.HttpClientProvider;
 import demo.multiagent.domain.AgentResponse;
-import dev.langchain4j.agent.tool.Tool;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
