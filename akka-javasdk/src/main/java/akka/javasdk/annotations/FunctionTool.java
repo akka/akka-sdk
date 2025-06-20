0fc1565c25ad74c26d7ab7a965462a7acb89d8d5
Renato Cavalcanti <renato@cavalcanti.io>
2025-06-20 20:33:34 +0200

    documentation and sample

--- akka-javasdk/src/main/java/akka/javasdk/annotations/FunctionTool.java
+++ akka-javasdk/src/main/java/akka/javasdk/annotations/FunctionTool.java
@@ -13,6 +13,9 @@ import java.lang.annotation.Target;
 /**
  * Annotation to expose methods as tools for use by an Agent.
  * Methods annotated with {@code FunctionTool} can be discovered and invoked by the Agent as callable tools.
+ * <p>
+ * It's recommended to annotate the method parameters with {@link Description} and provide a clear description.
+ * Parameters of type {@code Optional} are considered as non-required.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
