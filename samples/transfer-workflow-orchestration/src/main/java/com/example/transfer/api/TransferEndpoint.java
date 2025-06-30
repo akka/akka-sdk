881fb0d334655af7f06bbfdf48a52d6b279e4153
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2025-06-30 12:14:22 +0200

    docs: external service orchestration sample
    java docs
    
    Update samples/transfer-workflow-orchestration/README.md
    
    Co-authored-by: Renato Cavalcanti <renato@cavalcanti.be>
    
    improving smaple
    
    docs, improving compenstation sample
    
    PR comments

--- samples/transfer-workflow-orchestration/src/main/java/com/example/transfer/api/TransferEndpoint.java
+++ samples/transfer-workflow-orchestration/src/main/java/com/example/transfer/api/TransferEndpoint.java
@@ -8,14 +8,15 @@ import akka.javasdk.annotations.http.Post;
 import akka.javasdk.client.ComponentClient;
 import akka.javasdk.http.HttpResponses;
 import com.example.transfer.application.TransferWorkflow;
-import com.example.transfer.domain.TransferState.Transfer;
+import com.example.transfer.domain.Transfer;
+import com.example.transfer.domain.TransferState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // Opened up for access from the public internet to make the sample service easy to try out.
 // For actual services meant for production this must be carefully considered, and often set more limited
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
-@HttpEndpoint("/transfer")
+@HttpEndpoint
 public class TransferEndpoint {
 
   private static final Logger log = LoggerFactory.getLogger(TransferEndpoint.class);
@@ -26,27 +27,28 @@ public class TransferEndpoint {
     this.componentClient = componentClient;
   }
 
-  @Get("/{id}")
-  public String get(String id) {
-    log.info("Get transfer with id [{}].", id);
-    var transferState = componentClient.forWorkflow(id)
-      .method(TransferWorkflow::getTransferState).invoke();
-    return transferState.status().toString();
-  }
-
-  @Post("/{id}")
+  @Post("/transfer/{id}")
   public HttpResponse start(String id, Transfer transfer) {
     log.info("Starting transfer [{}].", transfer);
     componentClient.forWorkflow(id)
-      .method(TransferWorkflow::startTransfer).invoke(transfer);
+      .method(TransferWorkflow::start)
+      .invoke(transfer);
     return HttpResponses.accepted();
   }
 
-  @Post("/{id}/accept")
+  @Post("/transfer/{id}/accept")
   public HttpResponse accept(String id) {
     log.info("Accepting transfer [{}].", id);
     componentClient.forWorkflow(id)
-      .method(TransferWorkflow::accept).invoke();
+      .method(TransferWorkflow::accept)
+      .invoke();
     return HttpResponses.accepted();
+  }
+
+  @Get("/transfer/{id}")
+  public TransferState get(String id) {
+    return componentClient.forWorkflow(id)
+      .method(TransferWorkflow::get)
+      .invoke();
   }
 }
