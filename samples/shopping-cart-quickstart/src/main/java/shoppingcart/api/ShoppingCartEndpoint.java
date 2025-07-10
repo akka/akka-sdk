c597c1b60c6c35a09b62f27acc79223c5614ce0b
Renato Cavalcanti <renato@cavalcanti.io>
2025-07-10 07:42:27 +0200

    applied formatting

--- samples/shopping-cart-quickstart/src/main/java/shoppingcart/api/ShoppingCartEndpoint.java
+++ samples/shopping-cart-quickstart/src/main/java/shoppingcart/api/ShoppingCartEndpoint.java
@@ -22,7 +22,7 @@ import shoppingcart.domain.ShoppingCart;
 // tag::class[]
 
 // Opened up for access from the public internet to make the sample service easy to try out.
-// For actual services meant for production this must be carefully considered, 
+// For actual services meant for production this must be carefully considered,
 // and often set more limited
 // tag::endpoint-component-interaction[]
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
