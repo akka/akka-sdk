70e4679c93f5f9bb8e9c016dba30e99d200da718
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-09-01 12:07:04 +0200

    build: Release parent pom with sbt
    * explicit ci-release of parent pom from CI, otherwise not uploaded
    * updated ci-release plugin, to use central portal
    * missing license in pom

--- project/PublishPlugin.scala
+++ project/PublishPlugin.scala
@@ -3,7 +3,6 @@ import com.jsuereth.sbtpgp.PgpKeys.publishSigned
 import sbt._
 import sbt.Keys._
 import sbtdynver.DynVerPlugin.autoImport.dynverSonatypeSnapshots
-import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
 
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -33,7 +32,6 @@ object Publish extends AutoPlugin {
     Seq(
       publish / skip := false, // re-enable publishing
       dynverSonatypeSnapshots := true, // append -SNAPSHOT
-      sonatypeProfileName := "com.typesafe",
       beforePublishTask := beforePublish(isSnapshot.value),
       publishSigned := publishSigned.dependsOn(beforePublishTask).value,
       publishTo :=
