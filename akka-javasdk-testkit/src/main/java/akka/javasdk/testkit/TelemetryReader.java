942ec09d7aa6f9b31943eef4ba01944cf8192a5d
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2025-09-10 15:37:27 +0200

    different apporach to collecting spans

--- akka-javasdk-testkit/src/main/java/akka/javasdk/testkit/TelemetryReader.java
+++ akka-javasdk-testkit/src/main/java/akka/javasdk/testkit/TelemetryReader.java
@@ -6,6 +6,8 @@ package akka.javasdk.testkit;
 
 import static java.util.Optional.ofNullable;
 
+import akka.javasdk.annotations.ComponentId;
+import akka.javasdk.workflow.Workflow;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import java.util.Comparator;
@@ -21,9 +23,21 @@ public class TelemetryReader {
     this.inMemorySpanExporter = inMemorySpanExporter;
   }
 
+  public List<String> getWorkflowSteps(Class<? extends Workflow<?>> workflow, String workflowId) {
+    String componentId = getComponentId(workflow);
+    List<SpanData> spanDatas =
+        spansFor(componentId, AttributeKey.stringKey("akka.workflow.id"), workflowId);
+
+    return workflowStepsFrom(spanDatas);
+  }
+
   public List<String> getWorkflowSteps(String debugId) {
     List<SpanData> spanDatas = spansFor(debugId);
 
+    return workflowStepsFrom(spanDatas);
+  }
+
+  private List<String> workflowStepsFrom(List<SpanData> spanDatas) {
     AttributeKey<String> workflowStepNameAttribute =
         AttributeKey.stringKey("akka.workflow.step.name");
 
@@ -35,11 +49,42 @@ public class TelemetryReader {
         .toList();
   }
 
+  private String getComponentId(Class<?> componentClass) {
+    return componentClass.getAnnotation(ComponentId.class).value();
+  }
+
   private List<SpanData> collectByAttribute(
       List<SpanData> spanDatas, AttributeKey<String> attributeKey) {
     return spanDatas.stream()
         .filter(spanData -> spanData.getAttributes().get(attributeKey) != null)
         .toList();
+  }
+
+  private List<SpanData> spansFor(
+      String componentId, AttributeKey<String> idAttributeKey, String id) {
+    Optional<SpanData> mainSpan =
+        inMemorySpanExporter.getFinishedSpanItems().stream()
+            .filter(
+                spanData -> {
+                  return ofNullable(
+                              spanData
+                                  .getAttributes()
+                                  .get(AttributeKey.stringKey("akka.component.id")))
+                          .map(value -> value.equals(componentId))
+                          .orElse(false)
+                      && ofNullable(spanData.getAttributes().get(idAttributeKey))
+                          .map(value -> value.equals(id))
+                          .orElse(false);
+                })
+            .findFirst();
+
+    return mainSpan
+        .map(
+            spanData ->
+                inMemorySpanExporter.getFinishedSpanItems().stream()
+                    .filter(span -> span.getTraceId().equals(spanData.getTraceId()))
+                    .toList())
+        .orElse(List.of());
   }
 
   private List<SpanData> spansFor(String debugId) {
