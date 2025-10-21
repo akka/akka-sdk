5aaa8e0d3fcffe21c7ebd625934c458110e15cde
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-10-21 11:50:04 +0200

    improve docs

--- samples/doc-snippets/src/main/java/com/example/replicationfilter/ShoppingCartEntity.java
+++ samples/doc-snippets/src/main/java/com/example/replicationfilter/ShoppingCartEntity.java
@@ -5,14 +5,26 @@ package com.example.replicationfilter;
 
 // tag::replication-filter[]
 import akka.Done;
-import akka.javasdk.annotations.ComponentId;
+import akka.javasdk.annotations.Component;
 import akka.javasdk.annotations.EnableReplicationFilter;
 import akka.javasdk.eventsourcedentity.EventSourcedEntity;
 import akka.javasdk.eventsourcedentity.ReplicationFilter;
 
-@ComponentId("shopping-cart")
+@Component(id = "shopping-cart")
 @EnableReplicationFilter // <1>
 public class ShoppingCartEntity extends EventSourcedEntity<ShoppingCart, ShoppingCartEvent> {
+
+  // end::replication-filter[]
+  // tag::replication-filter-event[]
+  public Effect<Done> createCart(String userId) {
+    var selfRegion = commandContext().selfRegion();
+    return effects()
+        .persist(new CartCreated(commandContext().entityId(), userId))
+        .updateReplicationFilter(ReplicationFilter.includeRegion(selfRegion))
+        .thenReply(__ -> Done.getInstance());
+  }
+  // end::replication-filter-event[]
+  // tag::replication-filter[]
 
   public Effect<Done> replicateTo(String region) {
     return effects()
@@ -33,3 +45,5 @@ public class ShoppingCartEntity extends EventSourcedEntity<ShoppingCart, Shoppin
 record ShoppingCart(String id) {}
 
 interface ShoppingCartEvent {}
+
+record CartCreated(String cartId, String userId) implements ShoppingCartEvent {}
