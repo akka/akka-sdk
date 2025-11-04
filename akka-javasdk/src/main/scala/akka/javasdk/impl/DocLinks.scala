dcfdfead3e9e7849af012b8428a223d944a7bd69
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-11-04 12:05:36 +0100

    chore: Rename doc links to sdk (#1114)

--- akka-javasdk/src/main/scala/akka/javasdk/impl/DocLinks.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/DocLinks.scala
@@ -17,23 +17,23 @@ private[javasdk] object DocLinks {
   val MessageBrokersPage = "operations/message-brokers.html"
 
   private[javasdk] val errorCodes = Map(
-    "AK-00112" -> "java/views.html#changing",
+    "AK-00112" -> "sdk/views.html#changing",
     "AK-00406" -> MessageBrokersPage,
-    "AK-00415" -> "java/consuming-producing.html#consume-from-event-sourced-entity",
+    "AK-00415" -> "sdk/consuming-producing.html#consume-from-event-sourced-entity",
     "AK-00416" -> MessageBrokersPage,
-    "AK-01206" -> "java/agents.html#configuring_tool_call_limits")
+    "AK-01206" -> "sdk/agents.html#configuring_tool_call_limits")
 
   // fallback if isn't defined in errorCodes
   private val errorCodeCategories = Map(
-    "AK-001" -> "java/views.html",
-    "AK-002" -> "java/key-value-entities.html",
-    "AK-003" -> "java/event-sourced-entities.html",
-    "AK-004" -> "java/consuming-producing.html",
-    "AK-007" -> "java/using-jwts.html",
-    "AK-008" -> "java/timed-actions.html",
-    "AK-009" -> "java/access-control.html",
-    "AK-010" -> "java/workflows.html",
-    "AK-012" -> "java/agents.html")
+    "AK-001" -> "sdk/views.html",
+    "AK-002" -> "sdk/key-value-entities.html",
+    "AK-003" -> "sdk/event-sourced-entities.html",
+    "AK-004" -> "sdk/consuming-producing.html",
+    "AK-007" -> "sdk/using-jwts.html",
+    "AK-008" -> "sdk/timed-actions.html",
+    "AK-009" -> "sdk/access-control.html",
+    "AK-010" -> "sdk/workflows.html",
+    "AK-012" -> "sdk/agents.html")
 
   def forErrorCode(code: String): Option[String] = {
     val page = errorCodes.get(code).orElse(errorCodeCategories.get(code.take("AK-000".length)))
