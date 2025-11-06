f1ff1b5e07431577370e3aa49a4a2b8039edfbb1
Peter Vlugter <59895+pvlugter@users.noreply.github.com>
2025-11-06 18:49:29 +1300

    deprecated constructors for replication filters

--- akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
@@ -593,7 +593,9 @@ private final class Sdk(
             keyValue = false,
             name = Reflect.readComponentName(clz),
             description = Reflect.readComponentDescription(clz),
-            provided = isProvided(clz))
+            provided = isProvided(clz),
+            replicationFilterEnabled = false // FIXME: add replication filters
+          )
 
       case clz if Reflect.isKeyValueEntity(clz) =>
         val componentId = Reflect.readComponentId(clz)
@@ -634,7 +636,9 @@ private final class Sdk(
             keyValue = true,
             name = Reflect.readComponentName(clz),
             description = Reflect.readComponentDescription(clz),
-            provided = false)
+            provided = false,
+            replicationFilterEnabled = false // FIXME: add replication filters
+          )
 
       case clz if Reflect.isWorkflow(clz) =>
         val componentId = Reflect.readComponentId(clz)
