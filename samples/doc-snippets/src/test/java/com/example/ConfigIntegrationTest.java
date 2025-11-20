dcc432da659b36e243418ef83bc1a8fa0b597c4b
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-11-20 08:41:33 +0100

    docs: Document application configuration

---
+++ samples/doc-snippets/src/test/java/com/example/ConfigIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import akka.javasdk.testkit.TestKit;
+import akka.javasdk.testkit.TestKitSupport;
+
+// tag::config[]
+public class ConfigIntegrationTest extends TestKitSupport {
+
+  @Override
+  protected TestKit.Settings testKitSettings() {
+    return TestKit.Settings.DEFAULT
+        .withAdditionalConfig(
+            """
+            akka.javasdk.agent.openai.api-key = n/a
+            """
+        );
+  }
+
+}
+// end::config[]
