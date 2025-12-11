8ffd6aafb351292dc64cda74e6eeac89589af445
Johan Andrén <johan.andren@lightbend.com>
2025-12-11 13:28:55 +0100

    fix: Use lazy service discovery in dev mode (#1204)
    * fix: Use lazy service discovery in dev mode
    
    * test coverage

--- akka-javasdk-tests/src/test/java/akkajavasdk/components/Bootstrap.java
+++ akka-javasdk-tests/src/test/java/akkajavasdk/components/Bootstrap.java
@@ -4,9 +4,12 @@
 
 package akkajavasdk.components;
 
+import akka.javasdk.DependencyProvider;
 import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Acl;
+import akka.javasdk.grpc.GrpcClientProvider;
 import akkajavasdk.components.keyvalueentities.user.ProdCounterEntity;
+import akkajavasdk.protocol.TestGrpcServiceClient;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +19,14 @@ import org.slf4j.LoggerFactory;
 public class Bootstrap implements ServiceSetup {
 
   private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+  private final TestGrpcServiceClient eagerlyCreatedClient;
+
+  public Bootstrap(GrpcClientProvider clientProvider) {
+    // indirect test coverage of lazy resolution of other grpc services in dev mode
+    // (no concrete test case, but would fail all tests if it did not work)
+    eagerlyCreatedClient =
+        clientProvider.grpcClientFor(TestGrpcServiceClient.class, "some-other-service");
+  }
 
   @Override
   public void onStartup() {
@@ -25,5 +36,22 @@ public class Bootstrap implements ServiceSetup {
   @Override
   public Set<Class<?>> disabledComponents() {
     return Set.of(ProdCounterEntity.class);
+  }
+
+  @Override
+  public DependencyProvider createDependencyProvider() {
+    return new DependencyProvider() {
+
+      @SuppressWarnings("unchecked")
+      @Override
+      public <T> T getDependency(Class<T> clazz) {
+        if (clazz == TestGrpcServiceClient.class) {
+          // not normal usage, here for test coverage of lazily resolving grpc clients
+          return (T) eagerlyCreatedClient;
+        } else {
+          throw new IllegalArgumentException("Unknown dependency type " + clazz.getName());
+        }
+      }
+    };
   }
 }
