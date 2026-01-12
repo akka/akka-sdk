6405db18b3d39e5f6c7970792c63cbe3077648ad
Patrik Nordwall <patrik.nordwall@gmail.com>
2026-01-12 10:46:39 +0100

    chore: Mention httpClient in IntegrationTest

--- samples/helloworld-agent/src/test/java/com/example/IntegrationTest.java
+++ samples/helloworld-agent/src/test/java/com/example/IntegrationTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
  * This is a skeleton for implementing integration tests for an Akka application built with the Akka
  * SDK.
  *
- * <p>It interacts with the components of the application using a componentClient (already
- * configured and provided automatically through injection).
+ * <p>It interacts with the components of the application using a componentClient or httpClient
+ * (already configured and provided automatically through injection).
  */
 public class IntegrationTest extends TestKitSupport {
 
@@ -26,6 +26,6 @@ public class IntegrationTest extends TestKitSupport {
   @Test
   public void test() throws Exception {
     // implement your integration tests here by calling your
-    // components by using the `componentClient`
+    // components by using the `componentClient` or endpoints using `httpClient`
   }
 }
