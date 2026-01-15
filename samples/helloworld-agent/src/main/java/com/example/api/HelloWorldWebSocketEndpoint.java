6c04cf1ed298b720c2ee8a99bc7a66f3336c73a0
Johan Andrén <johan@markatta.com>
2026-01-15 12:35:57 +0100

    Bidirectional agent sample

---
+++ samples/helloworld-agent/src/main/java/com/example/api/HelloWorldWebSocketEndpoint.java
@@ -0,0 +1,51 @@
+package com.example.api;
+
+import akka.javasdk.annotations.Acl;
+import akka.javasdk.annotations.http.Get;
+import akka.javasdk.annotations.http.HttpEndpoint;
+import akka.javasdk.client.ComponentClient;
+import akka.javasdk.http.AbstractHttpEndpoint;
+import akka.javasdk.http.HttpResponses;
+import akka.stream.javadsl.Flow;
+import com.example.application.StreamingHelloWorldAgent;
+
+// tag::class[]
+
+/**
+ * This is a simple Akka Endpoint that uses an agent and LLM to generate
+ * greetings in different languages. An HTTP client connects a websocket with a username
+ * in the path and then sends individual requests over the socket to get the response
+ * streamed from the agent.
+ */
+// Opened up for access from the public internet to make the service easy to try out.
+// For actual services meant for production this must be carefully considered,
+// and often set more limited
+@Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
+@HttpEndpoint
+public class HelloWorldWebSocketEndpoint extends AbstractHttpEndpoint  {
+
+  private final ComponentClient componentClient;
+
+  public HelloWorldWebSocketEndpoint(ComponentClient componentClient) {
+    this.componentClient = componentClient;
+  }
+
+
+  // end::class[]
+  // Note: using websocket in a deployed service requires additional steps, see the documentation
+  // tag::class[]
+  @Get("/hello-websocket/{user}")
+  public String hello(String user) {
+    return HttpResponses.textWebsocket(requestContext(), // <1>
+        Flow.of(String.class)
+            .flatMapConcat(requestText -> // <2>
+                componentClient
+                    .forAgent()
+                    .inSession(user)
+                    .tokenStream(StreamingHelloWorldAgent::greet)
+                    .source(requestText)
+                )
+        );
+  }
+}
+// end::class[]
---
+++ samples/helloworld-agent/src/main/java/com/example/application/StreamingHelloWorldAgent.java
@@ -0,0 +1,35 @@
+package com.example.application;
+
+import akka.javasdk.agent.Agent;
+import akka.javasdk.annotations.Component;
+
+// tag::class[]
+@Component(id = "hello-world-agent")
+public class StreamingHelloWorldAgent extends Agent {
+
+  private static final String SYSTEM_MESSAGE =
+    """
+    You are a cheerful AI assistant with a passion for teaching greetings in new language.
+
+    Guidelines for your responses:
+    - Start the response with a greeting in a specific language
+    - Always append the language you're using in parenthesis in English. E.g. "Hola (Spanish)"
+    - The first greeting should be in English
+    - In subsequent interactions the greeting should be in a different language than
+      the ones used before
+    - After the greeting phrase, add one or a few sentences in English
+    - Try to relate the response to previous interactions to make it a meaningful conversation
+    - Always respond with enthusiasm and warmth
+    - Add a touch of humor or wordplay when appropriate
+    - At the end, append a list of previous greetings
+    """.stripIndent();
+
+  public StreamEffect greet(String userGreeting) {
+    // prettier-ignore
+    return streamEffects()
+      .systemMessage(SYSTEM_MESSAGE)
+      .userMessage(userGreeting)
+        .thenReply();
+  }
+}
+// end::class[]
