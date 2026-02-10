0e851c0cd093524fa6174a66c243b6eab5ee7036
Johan Andrén <johan@markatta.com>
2026-02-10 11:45:54 +0100

    some cleanup, optimization of descriptor lookup

--- akka-javasdk/src/main/scala/akka/javasdk/impl/ConsumerDescriptorFactory.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/ConsumerDescriptorFactory.scala
@@ -37,42 +37,38 @@ private[impl] object ConsumerDescriptorFactory extends ComponentDescriptorFactor
       .flatMap { method =>
         method.getParameterTypes.headOption match {
           case Some(inputType) =>
-            val invoker = MethodInvoker(method)
-            if (inputType.isSealed) {
-              inputType.getPermittedSubclasses.toList
-                .flatMap(subClass => {
-                  serializer.contentTypesFor(subClass).map(typeUrl => typeUrl -> invoker)
-                })
-            } else if (classOf[GeneratedMessageV3].isAssignableFrom(inputType)) {
-              // special handling of protobuf message types
-              if (inputType == classOf[GeneratedMessageV3]) {
-                // Base GeneratedMessageV3 handler - resolve concrete proto types
-                val protoTypes = Reflect.resolveProtoEventTypes(component)
-                if (protoTypes.isEmpty) {
-                  throw new IllegalStateException(
-                    s"Consumer [${component.getName}] handler method [${method.getName}] accepts GeneratedMessageV3 " +
-                    "but no concrete proto event types could be resolved. Add @ProtoEventTypes to the consumer class " +
-                    "or to the source event sourced entity.")
-                }
-                protoTypes.flatMap { protoClass =>
-                  serializer.registerTypeHints(protoClass)
-                  serializer.contentTypesFor(protoClass).map(_ -> invoker)
+            try {
+              val invoker = MethodInvoker(method)
+              if (inputType.isSealed) {
+                inputType.getPermittedSubclasses.toList
+                  .flatMap(subClass => {
+                    serializer.contentTypesFor(subClass).map(typeUrl => typeUrl -> invoker)
+                  })
+              } else if (classOf[GeneratedMessageV3].isAssignableFrom(inputType)) {
+                // special handling of protobuf message types, catch all
+                if (inputType == classOf[GeneratedMessageV3]) {
+                  // Base GeneratedMessageV3 handler - resolve concrete proto types
+                  val protoTypes = Reflect.resolveProtoEventTypes(component)
+                  if (protoTypes.isEmpty) {
+                    throw new IllegalStateException(
+                      s"Consumer [${component.getName}] handler method [${method.getName}] accepts GeneratedMessageV3 " +
+                      "but no concrete proto event types could be resolved. Add @ProtoEventTypes to the consumer class " +
+                      "or to the source event sourced entity.")
+                  }
+                  protoTypes.flatMap { protoClass =>
+                    serializer.registerTypeHints(protoClass)
+                    serializer.contentTypesFor(protoClass).map(_ -> invoker)
+                  }
+                } else {
+                  // specific concrete proto message input types
+                  val descriptor = Reflect.protoDescriptorFor(inputType.asSubclass(classOf[GeneratedMessageV3]))
+                  Seq(AnySupport.DefaultTypeUrlPrefix + "/" + descriptor.getFullName -> invoker)
                 }
               } else {
-                val descriptor =
-                  try {
-                    inputType
-                      .getMethod("getDescriptor")
-                      .invoke(null)
-                      .asInstanceOf[com.google.protobuf.Descriptors.Descriptor]
-                  } catch unwrapInvocationTargetExceptionCatcher
-
-                Seq(AnySupport.DefaultTypeUrlPrefix + "/" + descriptor.getFullName -> invoker)
+                val typeUrls = serializer.contentTypesFor(inputType)
+                typeUrls.map(_ -> invoker)
               }
-            } else {
-              val typeUrls = serializer.contentTypesFor(inputType)
-              typeUrls.map(_ -> invoker)
-            }
+            } catch unwrapInvocationTargetExceptionCatcher
           case None =>
             // FIXME check if there is a validation for that already
             throw new IllegalStateException(
