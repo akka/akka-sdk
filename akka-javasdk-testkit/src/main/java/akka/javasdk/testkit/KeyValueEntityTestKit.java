e76803b95ecb546d6e099b358ec0b99ab80ce0f6
Johan Andrén <johan@markatta.com>
2026-04-17 16:19:02 +0200

    feat: Ability to specify initial Key Value Entity state in testkit

--- akka-javasdk-testkit/src/main/java/akka/javasdk/testkit/KeyValueEntityTestKit.java
+++ akka-javasdk-testkit/src/main/java/akka/javasdk/testkit/KeyValueEntityTestKit.java
@@ -30,6 +30,8 @@ import java.util.function.Supplier;
  */
 public class KeyValueEntityTestKit<S, E extends KeyValueEntity<S>> {
 
+  public static final String DEFAULT_TEST_ENTITY_ID = "testkit-entity-id";
+
   private final Class<?> stateClass;
   private S state;
   private boolean deleted;
@@ -43,6 +45,16 @@ public class KeyValueEntityTestKit<S, E extends KeyValueEntity<S>> {
     this.state = entity.emptyState();
     this.stateClass = Reflect.keyValueEntityStateType(entity.getClass());
     this.emptyState = state;
+    this.deleted = false;
+  }
+
+  private KeyValueEntityTestKit(String entityId, E entity, S initialState) {
+    this.entityId = entityId;
+    this.entity = entity;
+    this.stateClass = Reflect.keyValueEntityStateType(entity.getClass());
+    this.emptyState = entity.emptyState();
+    verifySerDerWithExpectedType(stateClass, initialState, entity);
+    this.state = initialState;
     this.deleted = false;
   }
 
@@ -63,7 +75,7 @@ public class KeyValueEntityTestKit<S, E extends KeyValueEntity<S>> {
    */
   public static <S, E extends KeyValueEntity<S>> KeyValueEntityTestKit<S, E> of(
       Function<KeyValueEntityContext, E> entityFactory) {
-    return of("testkit-entity-id", entityFactory);
+    return of(DEFAULT_TEST_ENTITY_ID, entityFactory);
   }
 
   /** Creates a new testkit instance from a user defined entity id and a KeyValueEntity Supplier. */
@@ -80,6 +92,52 @@ public class KeyValueEntityTestKit<S, E extends KeyValueEntity<S>> {
       String entityId, Function<KeyValueEntityContext, E> entityFactory) {
     TestKitKeyValueEntityContext context = new TestKitKeyValueEntityContext(entityId);
     return new KeyValueEntityTestKit<>(entityId, entityFactory.apply(context));
+  }
+
+  /**
+   * Creates a new testkit instance from a KeyValueEntity Supplier, starting with the given initial
+   * state instead of the entity's {@code emptyState()}. Subsequent {@code deleteEntity()} effects
+   * still reset the state to the entity's {@code emptyState()}.
+   *
+   * <p>A default test entity id will be automatically provided.
+   */
+  public static <S, E extends KeyValueEntity<S>> KeyValueEntityTestKit<S, E> ofEntityWithState(
+      Supplier<E> entityFactory, S initialState) {
+    return ofEntityWithState(ctx -> entityFactory.get(), initialState);
+  }
+
+  /**
+   * Creates a new testkit instance from a function KeyValueEntityContext to KeyValueEntity,
+   * starting with the given initial state instead of the entity's {@code emptyState()}. Subsequent
+   * {@code deleteEntity()} effects still reset the state to the entity's {@code emptyState()}.
+   *
+   * <p>A default test entity id will be automatically provided.
+   */
+  public static <S, E extends KeyValueEntity<S>> KeyValueEntityTestKit<S, E> ofEntityWithState(
+      Function<KeyValueEntityContext, E> entityFactory, S initialState) {
+    return ofEntityWithState(DEFAULT_TEST_ENTITY_ID, entityFactory, initialState);
+  }
+
+  /**
+   * Creates a new testkit instance from a user defined entity id and a KeyValueEntity Supplier,
+   * starting with the given initial state instead of the entity's {@code emptyState()}. Subsequent
+   * {@code deleteEntity()} effects still reset the state to the entity's {@code emptyState()}.
+   */
+  public static <S, E extends KeyValueEntity<S>> KeyValueEntityTestKit<S, E> ofEntityWithState(
+      String entityId, Supplier<E> entityFactory, S initialState) {
+    return ofEntityWithState(entityId, ctx -> entityFactory.get(), initialState);
+  }
+
+  /**
+   * Creates a new testkit instance from a user defined entity id and a function
+   * KeyValueEntityContext to KeyValueEntity, starting with the given initial state instead of the
+   * entity's {@code emptyState()}. Subsequent {@code deleteEntity()} effects still reset the state
+   * to the entity's {@code emptyState()}.
+   */
+  public static <S, E extends KeyValueEntity<S>> KeyValueEntityTestKit<S, E> ofEntityWithState(
+      String entityId, Function<KeyValueEntityContext, E> entityFactory, S initialState) {
+    TestKitKeyValueEntityContext context = new TestKitKeyValueEntityContext(entityId);
+    return new KeyValueEntityTestKit<>(entityId, entityFactory.apply(context), initialState);
   }
 
   /**
