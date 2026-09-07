d0839f449849ab88a0d681b5fa7c6305ad57dba5
lightbend-tools-secure <148421913+lightbend-tools-secure@users.noreply.github.com>
2026-09-07 14:19:57 +0200

    bump: proto-google-common-protos 2.75.0 (was 2.61.3) (#1845)
    Co-authored-by: Akka Bot <81730826+kalix-bot@users.noreply.github.com>

--- project/SamplesCompilationProject.scala
+++ project/SamplesCompilationProject.scala
@@ -15,7 +15,7 @@ object SamplesCompilationProject {
   private val LangChain4JVersion = "1.1.0"
   private val EvalkitVersion = "0.1.0"
   private val additionalDeps = Map(
-    "doc-snippets" -> Seq("com.google.api.grpc" % "proto-google-common-protos" % "2.61.3" % "protobuf"),
+    "doc-snippets" -> Seq("com.google.api.grpc" % "proto-google-common-protos" % "2.75.0" % "protobuf"),
     "spring-dependency-injection" -> Seq("org.springframework" % "spring-context" % "6.2.8"),
     "ask-akka-agent" -> Seq(
       "dev.langchain4j" % "langchain4j-open-ai" % LangChain4JVersion,
