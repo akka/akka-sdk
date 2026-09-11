503b101c863ab7ef33f0e1db3e25b75cb0b68c4e
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2026-09-11 14:43:43 +0200

    chore: runtime bump 1.6.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val ProtocolVersionMinor = 1
   }
 
-  val AkkaRuntimeVersion = sys.props.getOrElse("akka-runtime.version", "1.6.16-8-5f6024e9-SNAPSHOT")
+  val AkkaRuntimeVersion = sys.props.getOrElse("akka-runtime.version", "1.6.17")
 
   // NOTE: embedded SDK should have the AkkaVersion aligned, when updating RuntimeVersion, make sure to check
   // if AkkaVersion and AkkaHttpVersion are aligned
