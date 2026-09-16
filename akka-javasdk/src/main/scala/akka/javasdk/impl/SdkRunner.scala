3146685b439456410fc046d1f6f56c59eda8e0ef
Renato Cavalcanti <renato@cavalcanti.be>
2026-09-16 16:31:36 +0200

    bump: runtime version to 1.6.17 (#1887)
    * bump: runtime version to 1.6.17
    
    * dependencies aligment
    
    * artifact bom

--- akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
+++ akka-javasdk/src/main/scala/akka/javasdk/impl/SdkRunner.scala
@@ -247,7 +247,7 @@ object SdkRunner {
             new SpiDevObjectStorageFilesystemBucketConfig(name, directory)
           case "s3" =>
             val creds = parseDevS3Credentials(name, c)
-            new SpiDevObjectStorageS3BucketConfig(name, c.getString("bucket"), c.getString("region"), creds)
+            new SpiDevObjectStorageS3BucketConfig(name, c.getString("bucket"), c.getString("region"), creds, None, None)
           case "gcs" =>
             val creds = parseDevGcsCredentials(name, c)
             new SpiDevObjectStorageGcsBucketConfig(name, c.getString("bucket"), creds)
