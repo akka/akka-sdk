84cdea165011290a7d2cf8e59527f2739fbfedb8
Patrik Nordwall <patrik.nordwall@gmail.com>
2026-09-22 09:54:25 +0000

    bump: akka-runtime 1.6.17-62-04bb796d-SNAPSHOT

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val ProtocolVersionMinor = 1
   }
 
-  val AkkaRuntimeVersion = sys.props.getOrElse("akka-runtime.version", "1.6.17-52-0e4ff415-SNAPSHOT")
+  val AkkaRuntimeVersion = sys.props.getOrElse("akka-runtime.version", "1.6.17-62-04bb796d-SNAPSHOT")
 
   // NOTE: embedded SDK should have the AkkaVersion aligned, when updating RuntimeVersion, make sure to check
   // if AkkaVersion and AkkaHttpVersion are aligned
